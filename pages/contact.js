cfde45f68443a3c1fa7d1dcabad5b7e0d1498ee0
Thomas <pmthomas1991@gmail.com>
2022-10-15 18:40:57 +0530

    contact yess

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,37 @@
-import { useState } from "react";
-import SideNav from "../components/SideNav";
 
 
 
-const Contact=() => {
 
 
-return (
-  <div>
+const Contact = () => {
 
-  </div>
-)
+  return (
+    <div>
+     <h1>Contact Form</h1>
+    <form action={`${process.env.EMAIL}`} method="POST">
 
+      <label htmlFor="first">First Name</label>
+      <input type="text" placeholder="First Name"
+       id="first" name="first" required />
+
+      <label htmlFor="last">Last Name</label>
+      <input type="text" placeholder="Last Name" 
+      id="last" name="last" required />
+
+      <label htmlFor="email">Email</label>
+      <input type="email" placeholder="Email"
+       id="email" name="email" required />
+      
+      <label htmlFor="message">Message</label>
+      <textarea type="text" id="message" placeholder="Message"
+       name="message" required/>
+
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  )
 }
 
 export default Contact
+
+
