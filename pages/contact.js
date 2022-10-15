295720182faddf492b7403aae0033bde6349584d
Thomas <pmthomas1991@gmail.com>
2022-10-15 11:24:50 +0530

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,18 +1,17 @@
+import { useState } from "react";
 import SideNav from "../components/SideNav";
 
 
 
 const Contact=() => {
 
-    return (
-      <div>
-        <div className="border-4 border-yellow-300 rounded-xl
-            absolute z-50 top-[8.5rem]
-            ml-[0.3rem] h-[3rem] w-[9rem]"/> 
-            <SideNav/>   
-      </div>
-    
-    );
+
+return (
+  <div>
+
+  </div>
+)
+
 }
 
 export default Contact
