021cd1265496926a9384ba7284459c900c9712de
Thomas <pmthomas1991@gmail.com>
2022-10-18 08:51:39 +0530

    final

--- components/SideNav.js
+++ components/SideNav.js
@@ -6,14 +6,14 @@ const SideNav = () => {
     return (
        <div>
           <div className="rounded-br-3xl bg-gradient-to-r from-cyan-500 to-blue-500 shadow-lg shadow-black
-          fixed z-30 h-[4rem] w-[30rem] left-0 flex justify-center flex-col items-center 
-          font-semibold text-3xl pl-[8.5rem]">
+          fixed z-30 h-[3rem] w-[23rem] left-0 flex justify-center flex-col items-center 
+          font-semibold text-xl pl-[8.5rem]">
              <h1 className="fixed font-serif text-gray-200">Thomas Mathew</h1>
           </div>
         <div className="flex fixed top-0 left-0
          flex-col z-40 text-yellow-300 font-semibold text-xl space-y-[1rem]
          pt-[1rem] bg-gray-600
-         h-[10.3rem] px-[1.3rem] shadow-xl shadow-black drop-shadow-2xl">
+         h-[10rem] px-[1.3rem] shadow-xl shadow-black drop-shadow-2xl">
 
           <Link href="/"><a>Home</a></Link>  
           <Link href="/aboutme"><a className="hover:animate-pulse">About Me</a></Link>
