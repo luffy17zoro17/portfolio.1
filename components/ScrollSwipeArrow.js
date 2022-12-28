68b7917ed5f6007b8c0861b214134bbee636bb17
Thomas <pmthomas1991@gmail.com>
2022-12-28 06:47:37 +0530

    swipescroll color

--- components/ScrollSwipeArrow.js
+++ components/ScrollSwipeArrow.js
@@ -11,11 +11,11 @@ const ScrollSwipeArrow = () => {
               font-bold fixed bottom-[3rem] w-[100%] left-0 
               flex justify-center items-center 
               text-white text-lg lg:text-xl'>
-              <span className="bg-yellow-900 rounded-xl shadow-inner border-4 shadow-black px-[0.5vw]">
+              <span className="bg-black rounded-xl shadow-inner border-4 shadow-black px-[0.5vw]">
                 Swipe&nbsp;</span><b className='text-green-400 text-[1.8rem]'>&nbsp;/&nbsp;</b>&nbsp;
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               &nbsp;&nbsp;
-              <span className='rounded-xl bg-yellow-900 shadow-inner border-4 shadow-black px-[0.5vw]'>
+              <span className='rounded-xl bg-black shadow-inner border-4 shadow-black px-[0.5vw]'>
                 Scroll</span>  
           <ArrowDownwardIcon className="text-pink-700 z-40 absolute 
           scale-[180%] shadow-xl shadow-black" 
