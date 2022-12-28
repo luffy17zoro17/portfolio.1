6f1cd44378d9b3353a4212bbe97e5cf5cb009b87
Thomas <pmthomas1991@gmail.com>
2022-12-28 06:59:35 +0530

    color changed

--- components/ScrollSwipeArrow.js
+++ components/ScrollSwipeArrow.js
@@ -7,15 +7,17 @@ const ScrollSwipeArrow = () => {
 
     return (
       <div>
-        <div className='z-20 animate-bounce h-[6%] text-white text-gray-300
+        <div className='z-20 animate-bounce h-[6%] 
               font-bold fixed bottom-[3rem] w-[100%] left-0 
               flex justify-center items-center 
-              text-white text-lg lg:text-xl'>
-              <span className="bg-black rounded-xl shadow-inner border-4 shadow-black px-[0.5vw]">
+               text-lg lg:text-xl'>
+              <span className="bg-gray-200 rounded-xl shadow-inner border-4 border-black 
+              shadow-black px-[0.5vw]">
                 Swipe&nbsp;</span><b className='text-green-400 text-[1.8rem]'>&nbsp;/&nbsp;</b>&nbsp;
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               &nbsp;&nbsp;
-              <span className='rounded-xl bg-black shadow-inner border-4 shadow-black px-[0.5vw]'>
+              <span className='rounded-xl bg-gray-200 shadow-inner border-4 
+              border-black shadow-black px-[0.5vw]'>
                 Scroll</span>  
           <ArrowDownwardIcon className="text-pink-700 z-40 absolute 
           scale-[180%] shadow-xl shadow-black" 
