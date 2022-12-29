d9fc7aa3cda71b03e8ac7ec02b4870cb2fc004d7
Thomas <pmthomas1991@gmail.com>
2022-12-29 07:55:24 +0530

    adjustments

--- components/SideNav.js
+++ components/SideNav.js
@@ -17,34 +17,37 @@ const SideNav = () => {
          space-y-[0.4rem] p-3 px-[1rem]
         shadow-lg shadow-black border-r-[0.5rem] border-gray-300 z-60
         rounded-br-3xl font-serif
-        flex flex-col text-[1rem] font-bold text-pink-500 
+        flex flex-col text-[1rem] font-bold text-pink-300 
           sm:text-[1.2rem] md:text-[1.3rem] lg:text-[1.5rem]">
 
-          <Link href="/"><a className="hover:text-orange-400 hover:font-mono
+          <Link href="/"><a className="bg-black rounded-r-full     
+          hover:text-orange-400 hover:font-mono
           focus:ring focus:ring-violet-300 focus:rounded-full
           focus:shadow-inner focus:shadow-black 
           
           focus:pl-2">Home</a></Link>  
           <Link href="/aboutme">
-            <a className="hover:text-orange-400 
-            hover:font-mono 
+            <a className="bg-black rounded-r-full
+            hover:text-orange-400 hover:font-mono 
             focus:ring focus:ring-violet-300 focus:rounded-full
             focus:shadow-inner focus:shadow-black
              focus:italic focus:px-2">About Me</a></Link>
           <Link href="/myworks">
-            <a className="
+            <a className="pr-2 bg-black rounded-r-full
             hover:text-orange-400 hover:font-mono
             focus:ring focus:ring-violet-300 focus:rounded-full
             focus:shadow-inner focus:shadow-black
              focus:px-2 focus:italic">My Works</a></Link>  
           <Link href="/skills">
-            <a className="hover:text-orange-400 
+            <a className="bg-black rounded-r-full 
+            hover:text-orange-400 
             hover:font-mono focus:shadow-inner focus:shadow-black
             focus:ring focus:ring-violet-300 focus:rounded-full
              focus:italic focus:px-2">Skills</a></Link>   
 
           <Link href="/contact">
-            <a className="hover:text-orange-400
+            <a className="bg-black rounded-r-full 
+            hover:text-orange-400
             focus:ring focus:ring-violet-300 focus:rounded-full
              focus:px-2 focus:shadow-inner focus:shadow-black
              focus:italic hover:font-mono">Contact</a></Link>
