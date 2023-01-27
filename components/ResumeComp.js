92a817c488154c3e3a17a72d636401c6d3d27369
Thomas <pmthomas1991@gmail.com>
2023-01-28 04:06:29 +0530

    final change

--- components/ResumeComp.js
+++ components/ResumeComp.js
@@ -13,9 +13,9 @@ const ResumeComp = ()=>{
             title={`Resume`}
             lclassN={``}
             ldivclassN={`bg-gradient-to-r from-black via-red-900 to-black
-             hover:bg-red-400 px-3 rounded-r-full font-sans
+             hover:bg-red-400 px-3 font-sans
              shadow-xl shadow-black z-50
-            text-xl -rotate-90 fixed right-[-1.3rem] border-y-4
+            text-xl -rotate-90 fixed right-[-1.3rem] border-x-4
              text-pink-300 font-bold hover:text-orange-400 hover:font-mono
            hover:border-cyan-400 md:text-2xl lg:text-3xl`}
          />
