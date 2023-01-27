515cc5f476b203b400f22a16665f93aebbbb4aee
Thomas <pmthomas1991@gmail.com>
2023-01-28 03:53:19 +0530

    small fixez

--- components/ResumeComp.js
+++ components/ResumeComp.js
@@ -6,16 +6,17 @@ import LinkProp from "./props/LinkProp";
 
 const ResumeComp = ()=>{
     return (
-       <div className="fixed top-0 bottom-0 right-0
-         flex items-center z-50 cursor-pointer hover:text-blue-300">
+       <div className="fixed top-[11vh] bottom-0 right-0 z-50
+         flex cursor-pointer hover:text-blue-300">
          <LinkProp
             lhref={`${process.env.PRIVATE_RESUM_KEY}`}
             title={`Resume`}
             lclassN={``}
             ldivclassN={`bg-gradient-to-r from-black via-red-900 to-black
              hover:bg-red-400 px-3 rounded-r-full font-sans
-            text-xl -rotate-90 fixed right-[-1.3rem] border-y-4 shadow-xl
-            shadow-black text-pink-300 font-bold hover:text-orange-400 hover:font-mono
+             shadow-xl shadow-black z-50
+            text-xl -rotate-90 fixed right-[-1.3rem] border-y-4
+             text-pink-300 font-bold hover:text-orange-400 hover:font-mono
            hover:border-cyan-400 md:text-2xl lg:text-3xl`}
          />
        </div> 
