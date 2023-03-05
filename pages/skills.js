73af2170b729153d6ee5d6d9181910c4e7d74885
Thomas <pmthomas1991@gmail.com>
2023-03-06 01:01:29 +0530

    fixes done

--- pages/skills.js
+++ pages/skills.js
@@ -13,14 +13,14 @@ const Skills = () => {
 
        
         <h1 className="font-mono fixed z-30 w-[50%] flex justify-center
-            text-[1.2rem] font-bold mt-[2.6rem] text-white shadow-lg shadow-black
+            text-[1.2rem] font-bold mt-[1rem] text-white shadow-lg shadow-black
             bg-gradient-to-r from-transparent via-black to-transparent
-            tracking-tight py-1 md:text-[1.9rem] md:mt-[0rem]">
+            py-1 md:py-0 md:text-[1.9rem] md:mt-[0rem]">
             MY SKILLS:
           </h1> 
         <main className="flex justify-center">
           
-         
+          
           <div className="shadow 
            
            shadow-black flex justify-center">
@@ -28,26 +28,30 @@ const Skills = () => {
           <Image
           src="https://w0.peakpx.com/wallpaper/508/202/HD-wallpaper-lego-toys-bricks-portrait-display-colorful-lightbulb-light-bulb-white-background-3d-blocks-simple-background.jpg"
           layout="fill"
-          className="object-contain shadow-inner shadow-black h-screen"
+          className="object-contain shadow-inner shadow-black h-screen
+            blur-sm"
           alt=""/> 
           
           </div> 
            
-          <div className="text-[1.1rem] font-mono     
+          <div className="text-[1.1rem] font-mono z-10
            flex justify-center flex-col bg-white fixed bottom-0 right-0
            shadow-lg shadow-black md:pr-[0rem] md:items-center
            ">
 
             <span className='
-            z-10 flex-none text-sm shadow-inner shadow-black
+              text-sm shadow-inner shadow-black
               bg-white border-4 border-black lg:p-4'>Im reliving my&nbsp;
              <span className="text-white font-semibold shadow-md 
              shadow-black bg-violet-500">childhood
-            </span> with:</span>       
+            </span> with:&nbsp;</span>       
                    
           </div>
+          <div className="h-screen absolute w-full bg-black opacity-40 z-0
+           "/> 
           <div className="flex flex-col justify-center items-center 
-          h-screen">
+          h-screen z-10">
+           
           <SkillSet/> 
           </div>
           </main>
